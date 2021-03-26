216ae7c7fe9f12a3f2f8be674b72bde524f49fa9
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-03-26 02:14:07 +0100

    feat: added proper VoxelShapes

--- src/main/java/com/kaupenjoe/beercraft/block/FluidPipe.java
+++ src/main/java/com/kaupenjoe/beercraft/block/FluidPipe.java
@@ -1,26 +1,23 @@
 package com.kaupenjoe.beercraft.block;
 
-import com.kaupenjoe.beercraft.BeerCraft;
 import com.kaupenjoe.beercraft.tileentity.FluidPipeTile;
-import com.kaupenjoe.beercraft.tileentity.FluidTankTest;
 import com.kaupenjoe.beercraft.tileentity.ModTileEntities;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockModelRenderer;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.util.AxisRotation;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
@@ -30,11 +27,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import org.apache.logging.log4j.LogManager;
 import org.jetbrains.annotations.Nullable;
-import org.lwjgl.system.CallbackI;
 
-import java.awt.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 public class FluidPipe extends Block
 {
@@ -47,96 +41,52 @@ public class FluidPipe extends Block
     // EWS NUD
     // 000 000
 
-    // TODO: VoxelShapes
+    private final VoxelShape SHAPE_NONE = Block.makeCuboidShape(4, 4, 4, 12, 12, 12);
 
-    private final VoxelShape SHAPE_NONE = Stream.of(
-            Block.makeCuboidShape(4, 5, 5, 5, 11, 11),
-            Block.makeCuboidShape(5, 5, 11, 11, 11, 12),
-            Block.makeCuboidShape(5, 5, 4, 11, 11, 5),
-            Block.makeCuboidShape(11, 5, 5, 12, 11, 11),
-            Block.makeCuboidShape(5, 4, 5, 11, 5, 11),
-            Block.makeCuboidShape(5, 11, 5, 11, 12, 11),
-            Block.makeCuboidShape(5, 11, 4, 11, 12, 5),
-            Block.makeCuboidShape(4, 11, 5, 5, 12, 11),
-            Block.makeCuboidShape(11, 11, 5, 12, 12, 11),
-            Block.makeCuboidShape(5, 11, 11, 11, 12, 12),
-            Block.makeCuboidShape(4, 4, 5, 5, 5, 11),
-            Block.makeCuboidShape(5, 4, 11, 11, 5, 12),
-            Block.makeCuboidShape(11, 4, 5, 12, 5, 11),
-            Block.makeCuboidShape(11, 5, 11, 12, 11, 12),
-            Block.makeCuboidShape(11, 5, 4, 12, 11, 5),
-            Block.makeCuboidShape(5, 4, 4, 11, 5, 5),
-            Block.makeCuboidShape(4, 5, 11, 5, 11, 12),
-            Block.makeCuboidShape(4, 5, 4, 5, 11, 5)
-    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+    private final VoxelShape SHAPE_N = Block.makeCuboidShape(4, 4, 0, 12, 12, 12);
+    private final VoxelShape SHAPE_S = Block.makeCuboidShape(4, 4, 4, 12, 12, 16);
+    private final VoxelShape SHAPE_W = Block.makeCuboidShape(0, 4, 4, 12, 12, 12);
+    private final VoxelShape SHAPE_E = Block.makeCuboidShape(4, 4, 4, 16, 12, 12);
 
-    private final VoxelShape SHAPE_N = Stream.of(
-            Block.makeCuboidShape(4, 5, 1, 5, 11, 11),
-            Block.makeCuboidShape(5, 5, 11, 11, 11, 12),
-            Block.makeCuboidShape(11, 5, 1, 12, 11, 11),
-            Block.makeCuboidShape(5, 4, 1, 11, 5, 11),
-            Block.makeCuboidShape(5, 11, 1, 11, 12, 11),
-            Block.makeCuboidShape(5, 11, 0, 11, 12, 1),
-            Block.makeCuboidShape(4, 11, 1, 5, 12, 11),
-            Block.makeCuboidShape(11, 11, 1, 12, 12, 11),
-            Block.makeCuboidShape(5, 11, 11, 11, 12, 12),
-            Block.makeCuboidShape(4, 4, 1, 5, 5, 11),
-            Block.makeCuboidShape(5, 4, 11, 11, 5, 12),
-            Block.makeCuboidShape(11, 4, 1, 12, 5, 11),
-            Block.makeCuboidShape(11, 5, 11, 12, 11, 12),
-            Block.makeCuboidShape(11, 5, 0, 12, 11, 1),
-            Block.makeCuboidShape(5, 4, 0, 11, 5, 1),
-            Block.makeCuboidShape(4, 5, 11, 5, 11, 12),
-            Block.makeCuboidShape(4, 5, 0, 5, 11, 1)
-    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+    public FluidPipe(Properties properties)
+    {
+        super(properties);
+    }
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn,
                                BlockPos pos, ISelectionContext context)
     {
-        if((state.get(NEIGHBOURS) & 4) == 4) // NORTH ACTIVE
-        {
-            System.out.println("NORTH getSHAPE");
-            return VoxelShapes.combine(SHAPE_NONE, SHAPE_N, IBooleanFunction.AND);
+        VoxelShape COMBINED = SHAPE_NONE;
+
+        // NORTH ACTIVE
+        if((state.get(NEIGHBOURS) & 4) == 4) {
+            COMBINED = VoxelShapes.or(COMBINED, SHAPE_N);
         }
 
-        if((state.get(NEIGHBOURS) & 32) == 32) // EAST ACTIVE
-        {
-
+        // EAST ACTIVE
+        if((state.get(NEIGHBOURS) & 32) == 32) {
+            COMBINED = VoxelShapes.or(COMBINED, SHAPE_E);
         }
 
-        if((state.get(NEIGHBOURS) & 8) == 8) // SOUTH ACTIVE
-        {
-
+        // SOUTH ACTIVE
+        if((state.get(NEIGHBOURS) & 8) == 8) {
+            COMBINED = VoxelShapes.or(COMBINED, SHAPE_S);
         }
 
-        if((state.get(NEIGHBOURS) & 16) == 16) // WEST ACTIVE
-        {
-
+        // WEST ACTIVE
+        if((state.get(NEIGHBOURS) & 16) == 16) {
+            COMBINED = VoxelShapes.or(COMBINED, SHAPE_W);
         }
 
-        return SHAPE_NONE;
-    }
-
-    private static VoxelShape rotateShape(Direction from, Direction to, VoxelShape shape) {
-        VoxelShape[] buffer = new VoxelShape[]{ shape, VoxelShapes.empty() };
-
-        int times = (to.getHorizontalIndex() - from.getHorizontalIndex() + 4) % 4;
-        for (int i = 0; i < times; i++) {
-            buffer[0].forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> buffer[1] =
-                    VoxelShapes.or(buffer[1], VoxelShapes.create(1-maxZ, minY, minX, 1-minZ, maxY, maxX)));
-            buffer[0] = buffer[1];
-            buffer[1] = VoxelShapes.empty();
-        }
-
-        return buffer[0];
+        return COMBINED;
     }
 
     @Override
     public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit)
     {
         if(worldIn.isRemote())
-            return ActionResultType.SUCCESS;
+            return ActionResultType.PASS;
 
         if (player.getHeldItemMainhand().getItem() == Items.DIAMOND)
         {
@@ -152,12 +102,11 @@ public class FluidPipe extends Block
             return ActionResultType.SUCCESS;
         }
 
-        return ActionResultType.SUCCESS;
+        return ActionResultType.PASS;
     }
 
     @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world)
-    {
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return ModTileEntities.FLUID_PIPE.get().create();
     }
 
@@ -166,9 +115,10 @@ public class FluidPipe extends Block
     public BlockState getStateForPlacement(BlockItemUseContext context)
     {
         updateNeighbours(context.getWorld(), context.getPos(), this.getDefaultState());
-        return super.getStateForPlacement(context);
+        return this.getDefaultState().with(NEIGHBOURS, 0);
     }
 
+    // TODO: You can currently still place the Pipe inside the Player which doesn't update neighbours
     private void updateNeighbours(World worldIn, BlockPos pos, BlockState state)
     {
         if(worldIn.isRemote()) {
@@ -199,17 +149,12 @@ public class FluidPipe extends Block
         neighbours = (isBlockW) ? neighbours + 16 : neighbours;
         neighbours = (isBlockE) ? neighbours + 32 : neighbours;
 
-        // System.out.println("NEIGH: " + neighbours);
-
         worldIn.setBlockState(pos, state.with(NEIGHBOURS, neighbours), 11);
-        redrawTexture(worldIn, pos);
     }
 
-    // Should probably be renamed!
     private void redrawTexture(World world, BlockPos pos)
     {
         int level = getTankLevel(world, 0, pos, Direction.NORTH);
-
         world.setBlockState(pos, world.getBlockState(pos).with(TANK_LEVEL, level), 27);
     }
 
@@ -240,15 +185,14 @@ public class FluidPipe extends Block
 
     @Override
     public void onNeighborChange(BlockState state, IWorldReader world,
-                                 BlockPos pos, BlockPos neighbor)
-    {
+                                 BlockPos pos, BlockPos neighbor) {
         updateNeighbours(((World)world), pos, world.getBlockState(pos));
     }
 
     @Override
     public void neighborChanged(BlockState state, World worldIn, BlockPos pos,
-                                Block blockIn, BlockPos fromPos, boolean isMoving)
-    {
+                                Block blockIn, BlockPos fromPos, boolean isMoving) {
+
         if(blockIn instanceof FluidPipe) {
             updateNeighbours(worldIn, pos, worldIn.getBlockState(pos));
         }
@@ -260,11 +204,6 @@ public class FluidPipe extends Block
     public boolean hasTileEntity(BlockState state)
     {
         return true;
-    }
-
-    public FluidPipe(Properties properties)
-    {
-        super(properties);
     }
 
     @Override
